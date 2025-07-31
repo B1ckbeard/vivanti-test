e9307d1ce7bebd7759d5451afbad1e42bd1f5f2e
Dmitry <46893211+B1ckbeard@users.noreply.github.com>
2025-07-31 11:44:45 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vivanti-test/',
+  base: 'https://b1ckbeard.github.io/vivanti-test',
   plugins: [
     react(),
     tailwindcss(),
