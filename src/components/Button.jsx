136094b42b71a0cc750a279eb8fefa03970d6b55
Dmitry <46893211+B1ckbeard@users.noreply.github.com>
2025-07-31 13:06:14 +0300

    gh-pages-test

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -14,7 +14,7 @@ function Button({ type = null, onClick, disabled = false }) {
   };
 
   const icons = {
-    next: { src: "src/next.png", alt: "next", className: "ml-[12px]" },
+    next: { src: "assets/next.png", alt: "next", className: "ml-[12px]" },
     back: {
       src: "src/assets/back.png",
       alt: "back",
